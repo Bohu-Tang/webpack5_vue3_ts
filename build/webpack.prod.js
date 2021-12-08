208d4a1e946c4d3be18cf4d06ee763ea6297f6d7
肖鲁新 <losen_xiao@topsky.com>
2021-12-08 10:32:04 +0800

    引入打包分析工具

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,27 +1,42 @@
 const {merge} = require("webpack-merge");
 const webpack = require("webpack");
 const common = require("./webpack.common");
-const { resolve } = require('./utils.js')
+const {resolve} = require('./utils.js');
+// 引入打包分析工具
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = function (env, argv) {
-    const nodeEnv = env.dev ? "development" : env.test ? "test" : "production";
-    return merge(common, {
-        mode: "production",
-        devtool: "source-map",
-        module: {
-            rules:[],
-        },
-        plugins: [
-            new webpack.DefinePlugin({
-                "process.env.NODE_ENV": JSON.stringify(nodeEnv),
-                __VUE_OPTIONS_API__: true,
-                __VUE_PROD_DEVTOOLS__: false,
-            }),
-        ],
-        output:{
-            path: resolve("dist"),
-            filename: "js/[name].[hash].js",
-            chunkFilename: "js/[name].[hash].js"
-        }
+  // 打包分析工具配置
+  const analyzerPlugins = env.analyzer ? [
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+      openAnalyzer: false,
+      //   generateStatsFile: true,
+      reportFilename: resolve("./report/report.html"),
+      statsFilename: resolve("./report/stats.json"),
     })
+  ] : []
+  // 判断当前环境
+  const nodeEnv = env.dev ? "development" : env.test ? "test" : "production";
+  return merge(common, {
+    mode: "production",
+    devtool: "source-map",
+    module: {
+      rules: [],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(nodeEnv),
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false,
+      }),
+      // 打包分析插件
+      ...analyzerPlugins,
+    ],
+    output: {
+      path: resolve("dist"),
+      filename: "js/[name].[hash].js",
+      chunkFilename: "js/[name].[hash].js"
+    }
+  })
 }
