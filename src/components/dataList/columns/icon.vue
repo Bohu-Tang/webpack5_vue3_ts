70d2634433b2fbc2c88e88ba11ef87b89c480269
肖鲁新 <losen_xiao@topsky.com>
2022-01-10 14:32:11 +0800

    zancun

--- src/components/dataList/columns/icon.vue
+++ src/components/dataList/columns/icon.vue
@@ -1,25 +1,33 @@
 <template>
     <div class="icon">
-        <el-icon v-show="col.showIcon(scope.row)" :style="{ width: size || '20px', height: size || '20px' }">
+        <el-icon
+            v-show="col.showIcon(scope.row)"
+            :style="{ width: size || '20px', height: size || '20px' }"
+        >
             <component :is="icon" :style="{ width: size || '20px', height: size || '20px' }"></component>
         </el-icon>
     </div>
 </template>
 <script lang="ts" >
-import { defineComponent, toRefs } from "vue";
+import { defineComponent, toRefs, PropType } from "vue";
+interface col {
+    label: string,
+    prop: string,
+    type: string,
+    [propName: string]: any
+}
+
 export default defineComponent({
     name: "icon",
     props: {
         col: {
-            type: Object
+            type: Object as PropType<col>
         },
         scope: {
-            type: Object
+            type: Object as PropType<any>
         }
     },
     setup(prop: any) {
-        console.log(prop);
-        
         const { col, scope } = toRefs(prop)
         const { icon, size } = col.value
         return {
@@ -28,9 +36,13 @@ export default defineComponent({
             scope,
             size
         }
-
     }
 })
 </script>
 <style lang="scss">
+.icon {
+    .el-icon {
+        vertical-align: middle;
+    }
+}
 </style>
